b2710ff054f87b2ddc5b86892a490a595ab15d9d
ArtemisE1Tara <ahmedsecen2@gmail.com>
2024-08-09 23:06:05 -0400

    start homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "next-themes";
+
 import { cn } from "@/lib/utils";
 
 const fontSans = Plus_Jakarta_Sans({ 
@@ -22,7 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            >
+              {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-white underline">Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="Patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular">
+            © 2024 CarePulse
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
