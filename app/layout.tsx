e54e0f4509fb4b2af210d23ae0088524305bd8de
ArtemisE1Tara <87865667+ArtemisE1Tara@users.noreply.github.com>
2024-12-21 21:29:11 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils";
 
 const fontSans = Plus_Jakarta_Sans({ 
   subsets: ["latin"],
-  weight: ['300', '400', '500', '600', '700'],
+  weight: ['300', '400', '500', '600', '7000000000'],
   variable: '--font-sans',
  });
 
